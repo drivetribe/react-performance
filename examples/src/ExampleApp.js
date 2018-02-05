42ca365a6bd97ec83d1e22669f47ee1dd3ce0f89
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2018-02-05 14:15:03 +0000

    adding isMuted

--- examples/src/ExampleApp.js
+++ examples/src/ExampleApp.js
@@ -1,6 +1,6 @@
-import * as React from 'react'
+import * as React from 'react';
 
-import ReactPerformance from './dist/react-performance'
+import ReactPerformance from './dist/react-performance';
 
 class CustomComponent extends React.Component {
   render() {
@@ -10,24 +10,24 @@ class CustomComponent extends React.Component {
       onPressUseful,
       Br,
       Button,
-      View,
-    } = this.props
+      View
+    } = this.props;
     return (
       <View>
         <Button onClick={onPressStartStop}>Start/stop</Button>
-        <Br /><Br />
+        <Br />
+        <Br />
         <Button onClick={onPressWasted}>Render wasted</Button>
-        <Br /><Br />
+        <Br />
+        <Br />
         <Button onClick={onPressUseful}>Render useful</Button>
-        {Array
-          .from(Array(40000))
+        {Array.from(Array(40000))
           .map((_, index) => new Date(2018, 0, index + 1))
           .filter(date => date.toISOString().includes('2017'))
           .map(date => ({ date }))
-          .reduce(accumulator => accumulator, 'dope')
-        }
+          .reduce(accumulator => accumulator, 'dope')}
       </View>
-    )
+    );
   }
 }
 
@@ -39,37 +39,40 @@ class AnotherCustomComponent extends React.Component {
       onPressUseful,
       Br,
       Button,
-      View,
-    } = this.props
+      View
+    } = this.props;
     return (
       <View>
         <Button onClick={onPressStartStop}>Start/stop</Button>
-        <Br /><Br />
+        <Br />
+        <Br />
         <Button onClick={onPressWasted}>Render wasted</Button>
-        <Br /><Br />
+        <Br />
+        <Br />
         <Button onClick={onPressUseful}>Render useful</Button>
       </View>
-    )
+    );
   }
 }
 
 const MeasuredComponent = ReactPerformance.measure({
   isCollapsed: false,
+  isMuted: false,
   getId: 'example',
-  Component: CustomComponent,
-})
+  Component: CustomComponent
+});
 
 const AnotherMeasuredComponent = ReactPerformance.measure({
   isCollapsed: false,
+  isMuted: true,
   getId: 'example-2',
-  Component: AnotherCustomComponent,
-})
+  Component: AnotherCustomComponent
+});
 
 export default class ExampleApp extends React.Component {
-
-  state = {}
+  state = {};
   render() {
-    const { Br } = this.props
+    const { Br } = this.props;
     return (
       <React.Fragment>
         <MeasuredComponent
@@ -78,28 +81,29 @@ export default class ExampleApp extends React.Component {
           onPressStartStop={() => this.onPressStartStop()}
           onPressWasted={() => this.setState({ wasted: !this.state.wasted })}
           onPressUseful={() => this.setState({ useful: !this.state.useful })}
-          />
-        <Br /><Br /><Br />
+        />
+        <Br />
+        <Br />
+        <Br />
         <AnotherMeasuredComponent
           {...this.props}
           useful={this.state.useful}
           onPressStartStop={() => this.onPressStartStop()}
           onPressWasted={() => this.setState({ wasted: !this.state.wasted })}
           onPressUseful={() => this.setState({ useful: !this.state.useful })}
-          />
+        />
       </React.Fragment>
-    )
+    );
   }
 
-  isRecording = false
+  isRecording = false;
   onPressStartStop() {
     if (this.isRecording) {
-      ReactPerformance.printRecording()
-      this.isRecording = false
-      return
+      ReactPerformance.printRecording();
+      this.isRecording = false;
+      return;
     }
-    this.isRecording = true
-    ReactPerformance.startRecording()
+    this.isRecording = true;
+    ReactPerformance.startRecording();
   }
-
 }
